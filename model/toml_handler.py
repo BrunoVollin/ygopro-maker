c454d193c40692c23e22e2fad7ba95182bf174e4
bruno <bruno.3av@gmail.com>
2022-04-17 21:40:11 -0300

    first commit

---
+++ model/toml_handler.py
@@ -0,0 +1,21 @@
+import toml
+
+class TomlHandler:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def read_toml(self):
+        with open(self.file_name, 'r', encoding="utf-8") as f:
+            return toml.load(f)
+
+    def write_toml(self, data):
+        with open(self.file_name, 'w', encoding="utf-8") as f:
+            toml.dump(data, f)
+
+    def append_toml(self, data):
+        with open(self.file_name, 'a', encoding="utf-8") as f:
+            toml.dump(data, f)
+    
+    def delete_toml(self):
+        with open(self.file_name, 'w', encoding="utf-8") as f:
+            f.truncate()
